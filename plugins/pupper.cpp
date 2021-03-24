871d88be28802db1079fd6e33b9541273ec3e4c9
alexnavtt <alexnavtt@utexas.edu>
2021-03-23 22:25:35 -0500

    Fixed indexing bug on joint arrays

--- plugins/pupper.cpp
+++ plugins/pupper.cpp
@@ -22,19 +22,19 @@ namespace gazebo
         // Get the individual leg joints
         front_left_joints[0] = model->GetJoint("front_left_shoulder_1_joint");
         front_left_joints[1] = model->GetJoint("front_left_shoulder_2_joint");
-        front_left_joints[3] = model->GetJoint("front_left_elbow_joint");
+        front_left_joints[2] = model->GetJoint("front_left_elbow_joint");
 
         front_right_joints[0] = model->GetJoint("front_right_shoulder_1_joint");
         front_right_joints[1] = model->GetJoint("front_right_shoulder_2_joint");
-        front_right_joints[3] = model->GetJoint("front_right_elbow_joint");
+        front_right_joints[2] = model->GetJoint("front_right_elbow_joint");
 
         back_left_joints[0] = model->GetJoint("back_left_shoulder_1_joint");
         back_left_joints[1] = model->GetJoint("back_left_shoulder_2_joint");
-        back_left_joints[3] = model->GetJoint("back_left_elbow_joint");
+        back_left_joints[2] = model->GetJoint("back_left_elbow_joint");
 
         back_right_joints[0] = model->GetJoint("back_right_shoulder_1_joint");
         back_right_joints[1] = model->GetJoint("back_right_shoulder_2_joint");
-        back_right_joints[3] = model->GetJoint("back_right_elbow_joint");
+        back_right_joints[2] = model->GetJoint("back_right_elbow_joint");
 
         //Connect plugin to Gazebo world instance
         this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&PupperPlugin::onUpdate, this));
