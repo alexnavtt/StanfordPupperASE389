d433a3593509a62dddf25a5c0e0fd16ed8d07dee
alexnavtt <alexnavtt@utexas.edu>
2021-03-25 20:56:17 -0500

    Added torque control and feedback

--- plugins/pupper.cpp
+++ plugins/pupper.cpp
@@ -4,46 +4,115 @@
 
 using std::cout;
 using std::endl;
+using std::vector;
 
 namespace gazebo
 {
+    // Load the model
     void PupperPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
     {
         // Safety check
         if (_model->GetJointCount() != 12)
         {
-            cout << "Invalid joint count, Scrat plugin not loaded" << endl;
+            cout << "Invalid joint count, Pupper plugin not loaded" << endl;
             return;
         }
 
         // Store the model pointer
-        model = _model;
+        model_ = _model;
 
         // Get the individual leg joints
-        front_left_joints[0] = model->GetJoint("front_left_shoulder_1_joint");
-        front_left_joints[1] = model->GetJoint("front_left_shoulder_2_joint");
-        front_left_joints[2] = model->GetJoint("front_left_elbow_joint");
+        front_left_joints_[0]  = model_->GetJoint("front_left_shoulder_1_joint");
+        front_left_joints_[1]  = model_->GetJoint("front_left_shoulder_2_joint");
+        front_left_joints_[2]  = model_->GetJoint("front_left_elbow_joint");
 
-        front_right_joints[0] = model->GetJoint("front_right_shoulder_1_joint");
-        front_right_joints[1] = model->GetJoint("front_right_shoulder_2_joint");
-        front_right_joints[2] = model->GetJoint("front_right_elbow_joint");
+        front_right_joints_[0] = model_->GetJoint("front_right_shoulder_1_joint");
+        front_right_joints_[1] = model_->GetJoint("front_right_shoulder_2_joint");
+        front_right_joints_[2] = model_->GetJoint("front_right_elbow_joint");
 
-        back_left_joints[0] = model->GetJoint("back_left_shoulder_1_joint");
-        back_left_joints[1] = model->GetJoint("back_left_shoulder_2_joint");
-        back_left_joints[2] = model->GetJoint("back_left_elbow_joint");
+        back_right_joints_[0]  = model_->GetJoint("back_right_shoulder_1_joint");
+        back_right_joints_[1]  = model_->GetJoint("back_right_shoulder_2_joint");
+        back_right_joints_[2]  = model_->GetJoint("back_right_elbow_joint");
 
-        back_right_joints[0] = model->GetJoint("back_right_shoulder_1_joint");
-        back_right_joints[1] = model->GetJoint("back_right_shoulder_2_joint");
-        back_right_joints[2] = model->GetJoint("back_right_elbow_joint");
+        back_left_joints_[0]   = model_->GetJoint("back_left_shoulder_1_joint");
+        back_left_joints_[1]   = model_->GetJoint("back_left_shoulder_2_joint");
+        back_left_joints_[2]   = model_->GetJoint("back_left_elbow_joint");
+
+        // Also collect all joints into a single array
+        for (int i = 0; i < 3; i++){
+            all_joints_[i + 3*FRONT_LEFT_LEG]  = front_left_joints_[i];
+            all_joints_[i + 3*FRONT_RIGHT_LEG] = front_right_joints_[i];
+            all_joints_[i + 3*BACK_RIGHT_LEG]  = back_right_joints_[i];
+            all_joints_[i + 3*BACK_LEFT_LEG]   = back_left_joints_[i];
+        }
 
         //Connect plugin to Gazebo world instance
-        this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&PupperPlugin::onUpdate, this));
+        this->updateConnection_ = event::Events::ConnectWorldUpdateBegin(std::bind(&PupperPlugin::onUpdate, this));
+
+        // Set up update rate variables
+        update_interval_  = common::Time(0, common::Time::SecToNano(0.02));  // In the form common::Time(sec, nanoSec): 50Hz
+        last_update_time_ = common::Time::GetWallTime();
     }
 
 
+    // Control the joints on one leg
+    void PupperPlugin::controlJoints(enum PupperLegs leg, vector<float> torques){
+        switch(leg){
+        case FRONT_LEFT_LEG:
+            front_left_joints_[0]->SetForce(0, torques[0]);
+            front_left_joints_[1]->SetForce(0, torques[1]);
+            front_left_joints_[2]->SetForce(0, torques[2]);
+            break;
 
+        case FRONT_RIGHT_LEG:
+            front_right_joints_[0]->SetForce(0, torques[0]);
+            front_right_joints_[1]->SetForce(0, torques[1]);
+            front_right_joints_[2]->SetForce(0, torques[2]);
+            break;
+
+        case BACK_RIGHT_LEG:
+            back_right_joints_[0]->SetForce(0, torques[0]);
+            back_right_joints_[1]->SetForce(0, torques[1]);
+            back_right_joints_[2]->SetForce(0, torques[2]);
+            break;
+
+        case BACK_LEFT_LEG:
+            back_left_joints_[0]->SetForce(0, torques[0]);
+            back_left_joints_[1]->SetForce(0, torques[1]);
+            back_left_joints_[2]->SetForce(0, torques[2]);
+            break;
+        }
+    }
+
+
+    // Control all joints on the robot 
+    void PupperPlugin::controlAllJoints(vector<float> torques){
+        for (int i = 0; i < 12; i++){
+            all_joints_[i]->SetForce(0, torques[i]);
+        }
+    } 
+
+
+    // Get torque measurements from the joints
+    vector<float> PupperPlugin::getJointFeedback(){
+        vector<float> joint_torques(12);
+
+        for (uint8_t i = 0; i < 12; i++){
+            physics::JointWrench joint_wrench = all_joints_[i]->GetForceTorque(0);
+            joint_torques[i] = (float)joint_wrench.body1Torque.Z();
+        }
+
+        return joint_torques;        
+    }
+
+    // Called on every simulation time step
     void PupperPlugin::onUpdate(){
-
+        //Manage publisher update rate
+        if (common::Time::GetWallTime() - last_update_time_ > update_interval_)
+        {
+            getJointFeedback();
+            last_update_time_ = common::Time::GetWallTime();
+        }
     }
 
     // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
