0e0d1b26a5c905c57901f43a9f7ba4a33f75b13b
alexnavtt <alexnavtt@utexas.edu>
2021-04-15 12:49:03 -0500

    Added update controller function

--- src/PupperCplusplusController/PupperWBC.cpp
+++ src/PupperCplusplusController/PupperWBC.cpp
@@ -1,1 +1,13 @@
 #include "ase389/PupperWBC.hpp"
+
+using std::vector;
+using std::array;
+
+void PupperWBC::updateController(const array<float, 12>& joint_angles, 
+                                 const array<float, 12>& joint_velocities,
+                                 const array<float, 12>& joint_torques)
+{
+    joint_angles_     = joint_angles;
+    joint_velocities_ = joint_velocities;
+    joint_torques_    = joint_torques;
+}
