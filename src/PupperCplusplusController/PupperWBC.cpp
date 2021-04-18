b97907216fd5314154080c5d923e239528c7e413
alexnavtt <alexnavtt@utexas.edu>
2021-04-18 12:06:07 -0500

    Edited in preparation for RBDL

--- src/PupperCplusplusController/PupperWBC.cpp
+++ src/PupperCplusplusController/PupperWBC.cpp
@@ -1,17 +1,21 @@
 #include "ase389/PupperWBC.hpp"
+#include "rbdl/rbdl.h"
 
 using std::vector;
 using std::array;
 using std::string;
+
+using namespace RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
 
 // Constructor
 PupperWBC::PupperWBC(){
     // Init stuff here
 }
 
-void PupperWBC::updateController(const array<float, 12>& joint_angles, 
-                                 const array<float, 12>& joint_velocities,
-                                 const array<float, 12>& joint_torques)
+void PupperWBC::updateController(const array<float, NUM_JOINTS>& joint_angles, 
+                                 const array<float, NUM_JOINTS>& joint_velocities,
+                                 const array<float, NUM_JOINTS>& joint_torques)
 {
     joint_angles_     = joint_angles;
     joint_velocities_ = joint_velocities;
