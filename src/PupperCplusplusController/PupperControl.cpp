1a346404bdd2d3e2a5b9895a7b5f55c8e6c818d9
alexnavtt <alexnavtt@utexas.edu>
2021-04-28 15:50:18 -0500

    Testing task jacobian

--- src/PupperCplusplusController/PupperControl.cpp
+++ src/PupperCplusplusController/PupperControl.cpp
@@ -2,6 +2,8 @@
 #include "ase389/PupperWBC.hpp"
 
 using std::array;
+using std::cout;
+using std::endl;
 
 static std::string urdf_relative_path = "pupper_description/pupper.urdf";
 
@@ -25,6 +27,11 @@ int main(int argc, char** argv){
     // Add the task with priority 0 (highest)
     Pup.addTask(0, "COM_pos", &CoM_Position_Task);
     Pup.addTask(1, "COM_ori", &CoM_Orientation_Task);
+
+    RigidBodyDynamics::Math::MatrixNd G = RigidBodyDynamics::Math::MatrixNd::Zero(3, Pup.Pupper_.qdot_size);
+    const RigidBodyDynamics::Math::Vector3d body_contact_point_left(0.0, -.11, 0.0095);
+    RigidBodyDynamics::CalcPointJacobian(Pup.Pupper_, Pup.joint_angles_, Pup.Pupper_.GetBodyId("back_lower_left_link"), body_contact_point_left, G);
+    cout << "Foot Jacobian: \n" << G.transpose() << endl;
 
     // Find the necessary torque to achieve this task
     array<float, 12> Tau = Pup.calculateOutputTorque();
