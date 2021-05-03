2b4b1b709342c212edc94ddb17bf6530de49f647
alexnavtt <alexnavtt@utexas.edu>
2021-05-02 21:53:50 -0500

    Added a function to more easily print Matrices

--- src/PupperCplusplusController/PupperControl.cpp
+++ src/PupperCplusplusController/PupperControl.cpp
@@ -6,7 +6,24 @@ using std::array;
 using std::cout;
 using std::endl;
 
-static std::string urdf_relative_path = "pupper_description/pupper.urdf";
+// Set floating precision to 3 sig figs
+static Eigen::IOFormat f(3);
+
+// Note: For debug only, function is very inefficient
+static void printMatrix(Eigen::MatrixXd A, std::string title = ""){
+    
+    // Clean up values close to zero
+    for (Eigen::Index i = 0; i < A.rows(); i++){
+        for (Eigen::Index j = 0; j < A.cols(); j++){
+            if (abs(A(i,j)) < 1e-6) A(i,j) = 0;
+        }
+    }
+
+    // Print
+    if (title != "") cout << title << endl;
+    cout << A.format(f) << endl;
+}
+
 
 int main(int argc, char** argv){
     PupperWBC Pup;
@@ -27,20 +44,24 @@ int main(int argc, char** argv){
     CoM_Orientation_Task.task_weight = 1;
     CoM_Orientation_Task.targets = {0, 0, 0, 1};    // identity quaternion 
 
-    // Add the task with priority 0 (highest)
+    // Add the tasks with priority 0 and 1
     // Pup.addTask(0, "COM_pos", &CoM_Position_Task);
     // Pup.addTask(1, "COM_ori", &CoM_Orientation_Task);
 
+    // Contact Jacobian Test
     //                // X,Y,Z, Q1,Q2,Q3, BL1,BL2,BL3,    BR1,BR2,BR3,      FL1,FL2,FL3,      FR1,FR2,FR3     Q4
-    Pup.joint_angles_ << 0,0,0,   .7071068,.7071068,0,   0,0,0,           0,0,0,           0, 0,0,            0,0,0,      0; 
-    RigidBodyDynamics::Math::MatrixNd G = RigidBodyDynamics::Math::MatrixNd::Zero(3, Pup.Pupper_.qdot_size);
-    const RigidBodyDynamics::Math::Vector3d body_contact_point_left(0.02, 0.02, 0.02);
-    const char* body_name = "back_right_leg_hub";
-    // RigidBodyDynamics::CalcPointJacobian(Pup.Pupper_, Pup.joint_angles_, Pup.Pupper_.GetBodyId(body_name), body_contact_point_left, G);
-    
-    cout << body_name << " Jacobian, id: " << Pup.Pupper_.GetBodyId(body_name) << "\n" << G.transpose() << endl;
-    static Eigen::IOFormat f(3);
-    cout << "JOINT ANGLES: \n" << Pup.joint_angles_.transpose().format(f) << endl;
+    // Pup.joint_angles_ << 0,0,0,   .7071068,.7071068,0,   0,0,0,           0,0,0,           0, 0,0,            0,0,0,      0; 
+    // RigidBodyDynamics::Math::MatrixNd Jc = RigidBodyDynamics::Math::MatrixNd::Zero(3, Pup.Pupper_.qdot_size);
+    // const RigidBodyDynamics::Math::Vector3d body_contact_point_left(0.02, 0.02, 0.02);
+    // RigidBodyDynamics::CalcPointJacobian(Pup.Pupper_, Pup.joint_angles_, Pup.Pupper_.GetBodyId(body_name), body_contact_point_left, Jc);
+    // cout << "JOINT ANGLES: \n" << Pup.joint_angles_.transpose().format(f) << endl;
+
+    // Body Jacobian Test
+    // const char* body_name = "front_right_hub";
+    // RigidBodyDynamics::Math::MatrixNd Jb = RigidBodyDynamics::Math::MatrixNd::Zero(6, Pup.Pupper_.qdot_size);
+    // RigidBodyDynamics::CalcBodySpatialJacobian(Pup.Pupper_, Pup.joint_angles_, Pup.Pupper_.GetBodyId(body_name), Jb, true);
+    // printMatrix(Jb.rightCols(12).transpose(), "Jacobian:");
+
     // Find the necessary torque to achieve this task
     // array<float, 12> Tau = Pup.calculateOutputTorque();
 
@@ -49,5 +70,6 @@ int main(int argc, char** argv){
     //     cout << "Body " << Pup.Pupper_.GetBodyName(i) << " Joint q index: "<< Pup.Pupper_.mJoints[i].q_index << endl;
     //     cout << "                           DOF: " << Pup.Pupper_.mJoints[i].mDoFCount << endl;
     // }
+
     return 0;
 }
